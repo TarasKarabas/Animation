cb014a53a21be51afc9ad6d66e5408e4d2af7566
TarasKarabas <taraskyparenko@gmail.com>
2023-04-13 18:17:42 +1030

    Initial Commit

---
+++ Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Taras Kyparenko on 13/4/2023.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
